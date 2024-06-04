a94cbdab63634e906b37f016fa5d657951c6ce15
martin george <martingeorge191110@gmail.com>
2024-06-04 17:21:41 +0300

    adding repo into my remote repo

---
+++ src/Home/photo-main/photo.jsx
@@ -0,0 +1,8 @@
+
+const PhotosUrl = {
+    mobileCenter: './photo-main/capsule_616x353.jpg',
+    tvCenter: './photo-main/BENT0157_BENTALL_CENTRE_WEBSITE_Bentalls_Brands167.jpg',
+    laptopCenter: './photo-main/pngtree-laptop-in-store-with-shelves-and-display-screen-picture-image_2759144.jpg',
+}
+
+export default PhotosUrl
